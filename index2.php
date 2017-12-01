152d8bd3c8b92db789dcf84e2a640a0f194d0384
lokdi <loic.gourdeau@icloud.com>
2017-12-01 12:56:40 +0100

    premier commit test epaignes

---
+++ index2.php
@@ -0,0 +1,10 @@
+<?php include('inc/fonction.php'); ?>
+<?php $title = ''; ?>
+<?php $titreH1 = ''; ?>
+
+
+<?php include ('inc/header.php'); ?>
+<h1>Cours de GIT</h1>
+<h2>test repository</h2>
+<h3>test add commit push</h3>
+<?php include ('inc/footer.php'); ?>
